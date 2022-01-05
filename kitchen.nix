def906bd58bae53b888f39b7c7e20cb15b33fdaa
Jeremy Wall <jeremy@marzhillstudios.com>
2022-01-04 20:11:31 -0500

    Keep up with the cargo shas

--- kitchen.nix
+++ kitchen.nix
@@ -2,7 +2,7 @@
 with nixpkgs;
     rustPlatform.buildRustPackage {
         pname = "kitchen";
-        version = "0.0.2";
+        version = "0.1.0";
         src = gitignoreSrc.gitignoreSource ./.;
-        cargoSha256 = "sha256-aSw+BX90rmcagVOkLVEfjlqTi+dv4QVT7JPZQd3eKjA=";
+        cargoSha256 = "sha256-SCTyR2TN6gNRkDeJOPPJQ2vJg9ClkLx0RJuMLpUWYBY=";
     }
