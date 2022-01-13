364f3a3d80f2bf3d538bc54734c71633e7067819
Jeremy Wall <jeremy@marzhillstudios.com>
2022-01-13 18:07:01 -0500

    Initial skeleton for a wasm ui crate.

--- web/src/main.rs
+++ web/src/main.rs
@@ -11,7 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+mod web;
 
-pub async fn ui_main() {
-    panic!("Not implemented yet");
+fn main() {
+    dioxus::web::launch(web::ui);
 }
--- web/src/web.rs
+++ web/src/web.rs
@@ -11,7 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+use dioxus::prelude::*;
 
-pub async fn ui_main() {
-    panic!("Not implemented yet");
+pub fn ui(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div { "hello chefs!" }
+    })
 }
