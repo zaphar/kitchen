edf85edfc5a67b966203a76eec2df890f41df2bd
Jeremy Wall <jeremy@marzhillstudios.com>
2022-01-25 20:51:06 -0500

    We do not need to disable non_snake_case warnings anymore

--- web/src/web.rs
+++ web/src/web.rs
@@ -11,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#![allow(non_snake_case)]
-
 use crate::console_log;
 use reqwasm::http;
 use sycamore::context::{use_context, ContextProvider, ContextProviderProps};
