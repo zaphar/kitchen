8b3be6260b039a50a23ed56ae7636e3145326b10
Jeremy Wall <jeremy@marzhillstudios.com>
2022-02-19 18:23:36 -0500

    Fix release download version

--- kitchen.nix
+++ kitchen.nix
@@ -3,7 +3,7 @@ with nixpkgs;
     nixpkgs.stdenv.mkDerivation rec {
         name = "kitchen";
         src = fetchurl {
-            url = "https://github.com/zaphar/kitchen/releases/download/v0.1.0/kitchen-linux";
+            url = "https://github.com/zaphar/kitchen/releases/download/v0.2.0-nix/kitchen-linux";
             sha256 = "1f1lxw893r6afgkhizvhm4pg20qfw3kwf9kbzmkbcw0d21qsd9z2";
         };
 
