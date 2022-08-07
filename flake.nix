e1ba92938fd7d2c5688dbafcfc45c9acf93af674
Jeremy Wall <jeremy@marzhillstudios.com>
2022-08-07 16:24:47 -0400

    nix package updates and stabilization

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
         gitignore = { url = "github:hercules-ci/gitignore.nix"; flake = false; };
         flake-utils.url = "github:numtide/flake-utils";
         rust-overlay = {
-          url = "github:oxalica/rust-overlay";
+          url = "github:oxalica/rust-overlay?ref=stable";
           inputs.nixpkgs.follows = "nixpkgs";
         };
         naersk.url = "github:nix-community/naersk";
@@ -21,19 +21,24 @@
         in
         flake-utils.lib.eachDefaultSystem (system:
             let
-                overlays = [ rust-overlay.overlay ];
+                overlays = [ rust-overlay.overlays.default ];
                 pkgs = import nixpkgs { inherit system overlays; };
-                # TODO(jwall): Could this get by with minimal instead?
-                rust-wasm = pkgs.rust-bin.stable.latest.default.override {
+                rust-wasm = pkgs.rust-bin.stable."1.60.0".default.override {
                   extensions = [ "rust-src" ];
+                  # Add wasm32 as an extra target besides the native target.
                   targets = [ "wasm32-unknown-unknown" ];
                 };
-                naersk-lib = naersk.lib."${system}";
+                # make sure to use our rust-wasm build target as the rust toolchain
+                # in naersk.
+                naersk-lib = pkgs.callPackage naersk {
+                    rustc = rust-wasm;
+                    cargo = rust-wasm;
+                };
                 kitchenWasm = kitchenWasmGen {
                     inherit pkgs rust-wasm version;
                 };
                 kitchen = (kitchenGen {
-                    inherit pkgs version naersk-lib kitchenWasm;
+                    inherit pkgs version naersk-lib kitchenWasm rust-wasm;
                     # Because it's a workspace we need the other crates available as source
                     root = (pkgs.callPackage gitignore { }).gitignoreSource ./.;
                 });
@@ -52,7 +57,7 @@
                     program = "${kitchen}/bin/kitchen";
                 };
                 devShell = pkgs.mkShell {
-                    buildInputs = [ rust-wasm ] ++ (with pkgs; [wasm-bindgen-cli wasm-pack diesel-cli]);
+                    buildInputs = [ rust-wasm ] ++ (with pkgs; [wasm-bindgen-cli wasm-pack]);
                 };
             } 
         );
--- nix/kitchen/default.nix
+++ nix/kitchen/default.nix
@@ -4,13 +4,16 @@
  kitchenWasm,
  version,
  naersk-lib,
+ rust-wasm,
 }:
 with pkgs;
 (naersk-lib.buildPackage rec {
     pname = "kitchen";
     inherit version;
+    buildInputs = [ rust-wasm ];
     # However the crate we are building has it's root in specific crate.
     src = root;
+    nativeBuildInputs = if stdenv.isDarwin then [ xcbuild ] else [ ];
     cargoBuildOptions = opts: opts ++ ["-p" "${pname}" ];
     postPatch = ''
       mkdir -p web/dist
