ba68738dfa235b2b975572b2517b045928f89ece
Jeremy Wall <jeremy@marzhillstudios.com>
2022-09-01 14:37:28 -0400

    SQLite migrations

---
+++ kitchen/migrations/20220901233514_recipes_v1.up.sql
@@ -0,0 +1,5 @@
+-- Add up migration script here
+create table recipes(user_id TEXT NOT NULL, recipe_id TEXT NOT NULL, recipe_text TEXT,
+    constraint recipe_primary_key primary key (user_id, recipe_id));
+create table categories(user_id TEXT NOT NULL, category_text,
+    constraint category_primary_key primary key (user_id));
--- nix/devShell/default.nix
+++ nix/devShell/default.nix
@@ -2,5 +2,4 @@
 with pkgs;
 mkShell {
     buildInputs = (if stdenv.isDarwin then [ pkgs.darwin.apple_sdk.frameworks.Security ] else [ ]) ++ (with pkgs; [wasm-bindgen-cli wasm-pack llvm clang rust-wasm]);
-    #buildInputs = with pkgs; [wasm-bindgen-cli wasm-pack];
 }
