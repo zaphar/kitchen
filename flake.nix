5bb05c00ad73107383afc5317191a93e065193c3
Jeremy Wall <jeremy@marzhillstudios.com>
2023-02-04 12:30:02 -0500

    Bump version to 0.2.21

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
             kitchenGen = (import ./nix/kitchen/default.nix);
             kitchenWasmGen = (import ./nix/kitchenWasm/default.nix);
             moduleGen = (import ./nix/kitchen/module.nix);
-            version = "0.2.21";
+            version = "0.2.22";
         in
         flake-utils.lib.eachDefaultSystem (system:
             let
