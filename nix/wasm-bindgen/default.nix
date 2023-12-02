e80953e9876d8ec360e9badb6d97ae1cd2b55a94
Jeremy Wall <jeremy@marzhillstudios.com>
2023-12-02 15:29:47 -0500

    maint: upgrade wasm-bindgen version

--- nix/wasm-bindgen/default.nix
+++ nix/wasm-bindgen/default.nix
@@ -20,14 +20,14 @@ rustPlatform.buildRustPackage rec {
   pname = "wasm-bindgen-cli";
   # NOTE(jwall): This must exactly match the version of the wasm-bindgen crate
   # we are using.
-  version = "0.2.84";
+  version = "0.2.89";
 
   src = fetchCrate {
     inherit pname version;
-    sha256 = "sha256-0rK+Yx4/Jy44Fw5VwJ3tG243ZsyOIBBehYU54XP/JGk=";
+    sha256 = "sha256-IPxP68xtNSpwJjV2yNMeepAS0anzGl02hYlSTvPocz8=";
   };
 
-  cargoSha256 = "sha256-vcpxcRlW1OKoD64owFF6mkxSqmNrvY+y3Ckn5UwEQ50=";
+  cargoSha256 = "sha256-pBeQaG6i65uJrJptZQLuIaCb/WCQMhba1Z1OhYqA8Zc=";
 
   nativeBuildInputs = [ pkg-config ];
 
@@ -36,5 +36,5 @@ rustPlatform.buildRustPackage rec {
   nativeCheckInputs = [ nodejs ];
 
   # other tests require it to be ran in the wasm-bindgen monorepo
-  cargoTestFlags = [ "--test=interface-types" ];
+  cargoTestFlags = [ "--test=reference" ];
 }
