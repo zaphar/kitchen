550d92179b8e7a77a8945e642c5adcae4cf3233a
Jeremy Wall <jeremy@marzhillstudios.com>
2023-12-04 14:48:50 -0500

    docs: comments for the event handling in login

--- web/src/pages/login.rs
+++ web/src/pages/login.rs
@@ -31,7 +31,8 @@ pub fn LoginForm<'ctx, G: Html>(cx: Scope<'ctx>, sh: StateHandler<'ctx>) -> View
                 info!("Attempting login request");
                 let (username, password) = ((*username.get_untracked()).clone(), (*password.get_untracked()).clone());
                 // NOTE(jwall): This is required if we want to keep the below auth request from
-                // failing to send with blocked by browser.
+                // failing to send with blocked by browser. This is because it's on a click and
+                // the form tries to do a submit event and aborts our network request.
                 evt.prevent_default();
                 if username != "" && password != "" {
                     spawn_local_scoped(cx, async move {
